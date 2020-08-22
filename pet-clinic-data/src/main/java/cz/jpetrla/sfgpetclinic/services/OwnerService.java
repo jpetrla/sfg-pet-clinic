49efbc1d4a8e8907f34b376dc1a0aff5afe2da75
Jan Petrla <j.petrla@gmail.com>
2020-08-22 15:04:40 +0200

    Implement Find Owner. Closes #54.

--- pet-clinic-data/src/main/java/cz/jpetrla/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/cz/jpetrla/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package cz.jpetrla.sfgpetclinic.services;
 
 import cz.jpetrla.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
